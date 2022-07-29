9d9ef5b64d225b163973e4b866df9e92950aa923
matheush-a <mathzalmeida@gmail.com>
2022-07-29 16:48:14 -0300

    feat cards indexing

--- database/migrations/2022_07_29_130903_create_user_types_table.php
+++ database/migrations/2022_07_29_130903_create_user_types_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users_type', function (Blueprint $table) {
+        Schema::create('user_types', function (Blueprint $table) {
             $table->id();
             $table->string('description')->unique();
             $table->timestamps();
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users_type');
+        Schema::dropIfExists('user_types');
     }
 };
